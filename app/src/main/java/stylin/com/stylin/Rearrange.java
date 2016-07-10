1dcba622362a85af04665b3d48991fe345b97dce
Aleksander Dash <adash@stanford.edu>
2016-07-09 19:47:19 -0700

    Tertiary stuff for moving things

--- app/src/main/java/stylin/com/stylin/Rearrange.java
+++ app/src/main/java/stylin/com/stylin/Rearrange.java
@@ -5,7 +5,9 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class Rearrange extends AppCompatActivity {
 
@@ -25,6 +27,8 @@ public class Rearrange extends AppCompatActivity {
                     intent.getByteArrayExtra(MainActivity.IMAGE_FILE).length
             );
             _imv.setImageBitmap(_bitmap);
+            RelativeLayout rl = (RelativeLayout) findViewById(R.id.base);
+            rl.addView(_imv);
         }
     }
 }
